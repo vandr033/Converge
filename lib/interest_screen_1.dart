6109a02cbc73b71dd27e3b75fb78095fb4c014a9
Your Name <you@example.com>
2022-10-28 16:43:43 -0400

    edited children

--- lib/interest_screen_1.dart
+++ lib/interest_screen_1.dart
@@ -15,11 +15,14 @@ class InterestScreen1State extends State<InterestScreen1> {
         padding: const EdgeInsets.all(
           30, //try this and see how it looks
         ),
-        child: Column(//column containing all of our rows. 
-          children: [//I believe we need 9 rows total... 
-            Row(//row 1 - text 
+        child: Column(
+          //column containing all of our rows.
+          children: [
+            //I believe we need 9 rows total...
+            Row(
+              //row 1 - text
               children: const [
-                  Text(
+                Text(
                   "See anything you like?",
                   style: TextStyle(
                     fontSize: 30,
@@ -33,55 +36,63 @@ class InterestScreen1State extends State<InterestScreen1> {
               //want space between ours rows within our column.
               height: 30,
             ),
-            Row(//row 2 - buttons
-              children: [...],
-              ),
-            const SizedBox(
-              height: 30,
-            ),
-            Row(//row 3 - buttons
-              children: [...],
+            Row(
+              //row 2 - buttons
+              children: [],
             ),
             const SizedBox(
               height: 30,
             ),
-            Row(//row 4 - buttons
-              children: [...],
+            Row(
+              //row 3 - buttons
+              children: [],
             ),
             const SizedBox(
               height: 30,
             ),
-            Row(//row 5 - buttons
-              children: [...],
+            Row(
+              //row 4 - buttons
+              children: [],
             ),
             const SizedBox(
               height: 30,
             ),
-            Row(//row 6 - buttons
-              children: [...],
+            Row(
+              //row 5 - buttons
+              children: [],
             ),
             const SizedBox(
               height: 30,
             ),
-            Row(//row 7 - buttons
-              children: [...],
+            Row(
+              //row 6 - buttons
+              children: [],
             ),
             const SizedBox(
               height: 30,
             ),
-            Row(//row 8 - navigation circles 
-              children: [...],
+            Row(
+              //row 7 - buttons
+              children: [],
             ),
             const SizedBox(
               height: 30,
             ),
-            Row(//row 9 - "next" button
-              children: [...],
+            Row(
+              //row 8 - navigation circles
+              children: [],
             ),
             const SizedBox(
               height: 30,
             ),
-          ],//last row- end of children within column.
+            Row(
+              //row 9 - "next" button
+              children: [],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+          ], //last row- end of children within column.
         ),
       ),
     );
