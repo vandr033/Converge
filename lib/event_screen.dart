c4a0fbc68b4b797843d8df1c0b7caf0962711bd6
Your Name <tsumm007@fiu.edu>
2022-11-10 12:39:33 -0500

    wrapped host container into expanded widget & row

--- lib/event_screen.dart
+++ lib/event_screen.dart
@@ -69,6 +69,7 @@ class _PanelWidgetState extends State<PanelWidget> {
   File? image2;
   File? image3;
   File? image4;
+  bool vis = false;
 
   Future pickImage1() async {
     try {
@@ -257,81 +258,98 @@ class _PanelWidgetState extends State<PanelWidget> {
               ),
             ),
             SizedBox(height: 20),
-            Container(
-              color: Color(0xffD7D9D7),
-              padding: EdgeInsets.all(10),
-              /*decoration: BoxDecoration(borderRadius:
-                                    BorderRadius.all(Radius.circular(20)),*/
-              child: TypeAheadField<User?>(
-                //Here we use <User> because that is what we are autocompleting for.
-                hideOnEmpty: true,
-                //TypeAheadField - A TextField that displays a list of suggestions as the user types.
-                //hideSuggestionsOnKeyboardHide: false,
-                textFieldConfiguration: TextFieldConfiguration(
-                  decoration: InputDecoration(
-                    suffixIcon: Icon(Icons.search, color: Color(0xff828382)),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(width: 0, style: BorderStyle.none),
-                    ),
-                    hintText: 'Hosts: ',
-                    hintStyle: TextStyle(
-                        fontSize: 16.0,
-                        color: Color(0xff828382),
-                        fontWeight: FontWeight.w700),
-                    filled: true,
-                    fillColor: Colors.white,
-                    contentPadding: EdgeInsets.all(10),
-                  ),
-                ),
-                suggestionsBoxDecoration: const SuggestionsBoxDecoration(
-                  color: Color(0xffD7D9D7),
-                ),
-                suggestionsCallback:
-                    UserData.getSuggestions, //we get suggestions from UserData
-                itemBuilder: (context, User? suggestion) {
-                  final user = suggestion!;
-
-                  return ListTile(
-                    leading: Container(
-                      width: 60,
-                      height: 60,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                          image: NetworkImage(user.imageUrl),
-                          fit: BoxFit.fill,
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Color(0xffD7D9D7),
+                    padding: EdgeInsets.all(10),
+                    /*decoration: BoxDecoration(borderRadius:
+                                          BorderRadius.all(Radius.circular(20)),*/
+                    child: TypeAheadField<User?>(
+                      //Here we use <User> because that is what we are autocompleting for.
+                      hideOnEmpty: true,
+                      //TypeAheadField - A TextField that displays a list of suggestions as the user types.
+                      //hideSuggestionsOnKeyboardHide: false,
+                      textFieldConfiguration: TextFieldConfiguration(
+                        decoration: InputDecoration(
+                          suffixIcon:
+                              Icon(Icons.search, color: Color(0xff828382)),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide:
+                                BorderSide(width: 0, style: BorderStyle.none),
+                          ),
+                          hintText: 'Hosts: ',
+                          hintStyle: TextStyle(
+                              fontSize: 16.0,
+                              color: Color(0xff828382),
+                              fontWeight: FontWeight.w700),
+                          filled: true,
+                          fillColor: Colors.white,
+                          contentPadding: EdgeInsets.all(10),
                         ),
                       ),
-                    ),
-                    title: Text(user.name),
-                  );
-                },
-                noItemsFoundBuilder: (context) => Container(
-                  height: 50,
-                  child: Center(
-                    child: Text(
-                      'No Users Found.',
-                      style: TextStyle(fontSize: 15),
+                      suggestionsBoxDecoration: const SuggestionsBoxDecoration(
+                        color: Color(0xffD7D9D7),
+                      ),
+                      suggestionsCallback: UserData
+                          .getSuggestions, //we get suggestions from UserData
+                      itemBuilder: (context, User? suggestion) {
+                        final user = suggestion!;
+
+                        return ListTile(
+                          leading: Container(
+                            width: 60,
+                            height: 60,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                image: NetworkImage(user.imageUrl),
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                          title: Text(user.name),
+                        );
+                      },
+                      noItemsFoundBuilder: (context) => Container(
+                        height: 50,
+                        child: Center(
+                          child: Text(
+                            'No Users Found.',
+                            style: TextStyle(fontSize: 15),
+                          ),
+                        ),
+                      ),
+                      onSuggestionSelected: (User? suggestion) {
+                        final user =
+                            suggestion!; //the suggestion that we selected is stored in user variable.
+
+                        // Container(height:20, width:20,
+
+                        // Text(name))
+
+                        //this is the part where we say what we want to do in the selection... aka we need to put it in a container.
+
+                        // Navigator.of(context).push(MaterialPageRoute(
+                        //   builder: (context) => UserDetailPage(user: user)
+                      }
+                      /*
+                        Container(height:20, width:20,
+                        Text(suggestion.name;)*/
+
+                      //this is the part where we say what we want to do in the selection... aka we need to put it in a container.
+                      /*
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => UserDetailPage(user: user)
+                      )
+                      );*/
+                      ,
                     ),
                   ),
                 ),
-                onSuggestionSelected: (User? suggestion) {
-                  final user =
-                      suggestion!; //the suggestion that we selected is stored in user variable.
-
-                  /*
-                  Container(height:20, width:20,
-                  Text(suggestion.name;)*/
-                  
-                  //this is the part where we say what we want to do in the selection... aka we need to put it in a container.
-                  /*
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => UserDetailPage(user: user)
-                )
-                );*/
-                },
-              ),
+              ],
             ),
           ],
         ),
