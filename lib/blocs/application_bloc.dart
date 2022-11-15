dc1ff3fd2076b3c55f07bb33f4f6b2ae4f77e573
Your Name <tsumm007@fiu.edu>
2022-11-15 12:14:43 -0500

    added app bloc

---
+++ lib/blocs/application_bloc.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application/services/places_service.dart';
+
+import '../models/place_search.dart';
+
+class ApplicationBloc with ChangeNotifier {
+  final placesService = PlacesService();
+  late List<PlaceSearch> searchResults;
+
+  searchPlaces(String searchTerm) async {
+    searchResults = await placesService.getAutocomplete(searchTerm);
+    notifyListeners();
+  }
+}
--- lib/services/places_service.dart
+++ lib/services/places_service.dart
@@ -1,3 +1,25 @@
 /*
 Here we write a service so that we can get json from google places api and convert to dart object as defined in our model.
 */
+import 'package:flutter_application/models/place_search.dart';
+import 'package:http/http.dart' as http; //for http requests aka the google api
+import 'dart:convert'
+    as convert; //to convert between data representations aka json to dart object.
+
+class PlacesService {
+  final key = 'AIzaSyCjmZuiimm7QZSJmxvfTBJBSblMeHyG6UQ';
+
+  Future<List<PlaceSearch>> getAutocomplete(String search) async {
+    var url =
+        'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$search&inputtype=textquery&key=$key';
+
+    var response = await http.get(Uri.parse(url));
+    var json = convert.jsonDecode(response
+        .body); //jsonDecode parses string and returns resulting json object
+
+    //we want results as a list - we grab the predictions object, and then we get a list of all of the objects within it, aka, the suggestions.
+    var jsonResults = json['predictions'] as List;
+
+    return jsonResults.map((place) => PlaceSearch.fromJson(place)).toList();
+  }
+}
